9cbe23ea35957ded6a8a6703cdd3cf4f2b9dc36d
Liana Falah <falahpujimarliana@gmail.com>
2018-12-17 10:24:39 +0700

    no message

---
+++ TestAndroid/app/src/main/java/com/example/testandroid/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.testandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        Intent intent= new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void signup(View view){
+        Intent intent= new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ TestAndroid/app/src/main/java/com/example/testandroid/MenuActivity.java
@@ -0,0 +1,28 @@
+package com.example.testandroid;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+import android.widget.TableLayout;
+
+
+import com.example.testandroid.controller.DevPagerAdapter;
+
+public class MenuActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    private  ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        toolbar=(Toolbar) findViewById(R.id.toolbar_menu);
+
+        viewPager =findViewById(R.id.viewPager);
+        viewPager.setAdapter(new DevPagerAdapter(getSupportFragmentManager()));
+        TabLayout tabs = findViewById(R.id.tabLayoutId);
+         tabs.setupWithViewPager(viewPager);
+    }
+}
