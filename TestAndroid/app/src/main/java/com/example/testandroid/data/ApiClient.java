d5db61be87eb2eef8e6afba7b52d676d58180e67
Liana Falah <falahpujimarliana@gmail.com>
2018-12-17 12:21:32 +0700

    no message

--- TestAndroid/app/src/main/java/com/example/testandroid/data/ApiClient.java
+++ TestAndroid/app/src/main/java/com/example/testandroid/data/ApiClient.java
@@ -1,4 +1,18 @@
 package com.example.testandroid.data;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+    public static final String BASE_URL = "http://maxxapi.technopartner.rocks/api/login";
+    private static Retrofit retrofit = null;
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
