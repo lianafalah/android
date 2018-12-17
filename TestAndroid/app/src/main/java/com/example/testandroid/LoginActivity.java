5964e8db3e7d8651fd0b8cbeebebb93cee66e6ef
nicolas novendra <nico@oninyon.com>
2018-12-17 15:59:07 +0700

    -M: Implement login

--- TestAndroid/app/src/main/java/com/example/testandroid/LoginActivity.java
+++ TestAndroid/app/src/main/java/com/example/testandroid/LoginActivity.java
@@ -4,10 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.testandroid.data.ApiClient;
+import com.example.testandroid.data.ApiService;
+import com.example.testandroid.model.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
     Toolbar toolbar;
+    ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +27,29 @@ public class LoginActivity extends AppCompatActivity {
         toolbar=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        apiService = ApiClient.getClient().create(ApiService.class);
+
     }
     public void signup(View view){
         Intent intent= new Intent(LoginActivity.this, SignUpActivity.class);
         startActivity(intent);
+    }
+
+    public void login(View view) {
+        EditText email = findViewById(R.id.loginEmail);
+        EditText password = findViewById(R.id.loginPassword);
+
+        Call<LoginResponse> call = apiService.postLoginData(email.getText().toString(), password.getText().toString(), "Bearrer");
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                Log.i("### LIANA", "onResponse login: " + response.body().username);
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
