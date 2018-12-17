7dee99e6f8735b93c7fca50b50a8f1be93ec1029
Liana Falah <falahpujimarliana@gmail.com>
2018-12-17 11:15:07 +0700

    no message

--- TestAndroid/app/src/main/java/com/example/testandroid/MainActivity.java
+++ TestAndroid/app/src/main/java/com/example/testandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.testandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void login(View view){
+        Intent intent= new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void signup(View view){
+        Intent intent= new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
